257ea1fbd1477cf60aa66450ecd5d4f92dba17e5
milkboy2564 <milkboy2564@mcircle.biz>
2022-10-18 16:49:28 +0900

    feat: SideBar Component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,25 +1,50 @@
+import {
+  adminSideBarMenus,
+  boardSideBarMenus,
+  SideBarPath,
+  SIDE_BAR_ICONS,
+  userSideBarMenus,
+} from '../utils/const/sidebarMenus';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+
 import Toolbar from '@mui/material/Toolbar';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
-import { userSideBarMenus } from '../utils/const/sidebarMenus';
+// 사이드바의 가로 길이
+const SIDE_BAR_WIDTH = 240;
 
-const drawerWidth = 240;
+/*
+    TODO
+    -[O]: 관리자 페이지, 마이 페이지, 게시판 페이지를 검증하여 targetMenus에 할당 -> URL 이용
+    -[]: 현재 선택한 메뉴 스타일링 -> URL 이
+    -[]: 정확한 페이지 url 정해지면 라우팅 -> useRouter 사용
+*/
 
-export default function ResponsiveDrawer() {
-  const drawer = (
+export default function CustomSideBar() {
+  const [targetMenus, setTargetMenus] = useState<string[]>([]);
+  const router = useRouter();
+
+  const handleMenuClick = (menu: string) => {
+    router.push(SideBarPath[menu]);
+  };
+
+  const sidebar = (
     <div>
       <Toolbar />
       <List>
-        {userSideBarMenus.map((text: string) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              {/* <ListItemIcon>{userSideBarIcons[text]}</ListItemIcon> */}
-              <ListItemText primary={text} />
+        {targetMenus.map((menu: string) => (
+          <ListItem key={menu} disablePadding>
+            <ListItemButton onClick={() => handleMenuClick(menu)}>
+              <ListItemIcon>{SIDE_BAR_ICONS[menu]}</ListItemIcon>
+              <ListItemText primary={menu} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -27,18 +52,29 @@ export default function ResponsiveDrawer() {
     </div>
   );
 
+  // URL을 통해 현재 페이지에 맞는 사이드바 검증
+  useEffect(() => {
+    if (router.pathname === '/board') {
+      setTargetMenus(boardSideBarMenus);
+    } else if (router.pathname === '/mypage') {
+      setTargetMenus(userSideBarMenus);
+    } else if (router.pathname === '/management') {
+      setTargetMenus(adminSideBarMenus);
+    }
+  }, []);
+
   return (
     <Box sx={{ display: 'flex' }}>
-      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+      <Box component="nav" sx={{ width: { sm: SIDE_BAR_WIDTH }, flexShrink: { sm: 0 } }}>
         <Drawer
           variant="permanent"
           sx={{
             display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: SIDE_BAR_WIDTH },
           }}
           open
         >
-          {drawer}
+          {sidebar}
         </Drawer>
       </Box>
     </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import SideBar from '../components/SideBar';
-
 function Index() {
   //테스트이빈다
-  return (
-    <div>
-      <SideBar />
-    </div>
-  );
+  return <div>Index Page</div>;
 }
 
 export default Index;
