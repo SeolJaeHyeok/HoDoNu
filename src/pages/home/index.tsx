b74947dbdb832c2ac59aa5ea81eda0ae2ac05d15
milkboy2564 <milkboy2564@mcircle.biz>
2022-10-21 17:24:47 +0900

    fix: TP-15 Mock Data 주석 처리

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,193 +1,193 @@
 import styled from '@emotion/styled';
 
-const freeArticles = [
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 8678686,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 856798707,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 746765978,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 64363769870,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 4262468769780,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-];
+// const freeArticles = [
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 8678686,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 856798707,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 746765978,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 64363769870,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 4262468769780,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+// ];
 
-const doctorArticles = [
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 12313534534,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 434324234,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 43242423,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 4324235246,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 6454564,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-];
+// const doctorArticles = [
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 12313534534,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 434324234,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 43242423,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 4324235246,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 6454564,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+// ];
 
-const nurseArticles = [
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 432,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 3211,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 432342,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 43243242,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-  {
-    createdAt: '2022-10-21T07:57:43.146Z',
-    articleId: 53464564,
-    title: '안녕하세요',
-    hits: 0,
-    user: {
-      userId: '165f7945-b592-4271-b1e0-6df436ddc743',
-      email: 'dohee@test.com',
-      imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
-      nickname: 'frozen_apricot_gamefowl',
-    },
-  },
-];
+// const nurseArticles = [
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 432,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 3211,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 432342,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 43243242,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+//   {
+//     createdAt: '2022-10-21T07:57:43.146Z',
+//     articleId: 53464564,
+//     title: '안녕하세요',
+//     hits: 0,
+//     user: {
+//       userId: '165f7945-b592-4271-b1e0-6df436ddc743',
+//       email: 'dohee@test.com',
+//       imgUrl: 'https://toy-project-s3.s3.ap-northeast-2.amazonaws.com/user/basic/basic-Image.png',
+//       nickname: 'frozen_apricot_gamefowl',
+//     },
+//   },
+// ];
 
 export default function Home() {
   return (
