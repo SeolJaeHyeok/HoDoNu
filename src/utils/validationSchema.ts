d8e2d6b90453e3153ca09788d44d7975f80898a7
yeseul119 <yslee@mcircle.biz>
2022-10-26 09:52:44 +0900

    feat TP-17 create form UI 구현

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -4,3 +4,10 @@ export const loginValidationSchema = yup.object({
   email: yup.string().email('이메일 형식에 맞지 않습니다.').required('이메일은 필수 입력값입니다.'),
   password: yup.string().required('비밀번호는 필수 입력값입니다.'),
 });
+
+export const boardValidationSchema = yup.object({
+  images: yup.string(),
+  category: yup.string(),
+  title: yup.string(),
+  content: yup.string(),
+});
