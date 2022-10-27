b804bd68630e791d7f652e95e73664e705c8067a
milkboy2564 <milkboy2564@mcircle.biz>
2022-10-27 17:12:00 +0900

    feature: TP-18 모아보기 기능 및 URL 라우팅

--- src/pages/board/free/index.tsx
+++ src/pages/board/free/index.tsx
@@ -7,11 +7,12 @@ import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 /*
   TODO  
   -[O] API 연결 후 테스트
-  -[O] Pagination - 데이터 담아서 테스트 필요, perPage 설정 필요
+  -[O] Pagination - 데이터 많이 담아서 테스트 필요
   -[] 검색
   -[] 같은 양식으로 의사, 간호사 게시판 만들기
   -[] SSR 적용하기
@@ -21,12 +22,16 @@ export default function FreeBoard() {
   const router = useRouter();
   const [sort, setSort] = useState<string | string[] | undefined>(router.query.sort);
   const [page, setPage] = useState<string | string[] | undefined>(router.query.page);
-  const [perPage] = useState<string | string[] | undefined>(router.query.perPage);
+  const [perPage, setPerPage] = useState<string | string[] | undefined>(router.query.perPage);
 
   const { data: res } = useQuery(['board', 'free', sort, page, perPage], () =>
     boardApi.getAllFreeBoards({ page, perPage, sort })
   );
 
+  // 총 페이지 수
+  const TOTAL_PAGE = Math.ceil(res?.data.result.articles.length / Number(perPage));
+
+  // 최신 순, 조회순 정렬
   const handleSortClick = (sort: string) => {
     // Sort 정렬 기준 설정
     setSort(() => sort);
@@ -41,6 +46,7 @@ export default function FreeBoard() {
     });
   };
 
+  // Pagination - page
   const handlePageNavigate = (pageNumber: number) => {
     // Page 정렬 기준 설정
     setPage(() => String(pageNumber + 1));
@@ -55,10 +61,24 @@ export default function FreeBoard() {
     });
   };
 
+  // 모아보기 - perPage
+  const handlePerPage = (e: any) => {
+    // Per Page 정렬 기준 설정
+    setPerPage(() => e.target.value);
+
+    // 해당 값으로 URL 변경
+    router.push({
+      query: {
+        page,
+        perPage: e.target.value,
+        sort,
+      },
+    });
+  };
+
   return (
     <BoardContainer>
       <BoardHeader>
-        <SearchBar width={200} height={30} />
         <div>
           <FilterButton
             value={'최신순'}
@@ -71,12 +91,26 @@ export default function FreeBoard() {
             onClick={() => handleSortClick('Hits')}
           />
         </div>
+        <SearchBar width={200} height={30} />
+        <FormControl>
+          <InputLabel id="perpage-label"></InputLabel>
+          <Select
+            sx={{
+              height: '30px',
+              width: '150px',
+            }}
+            labelId="perpage-label"
+            value={perPage}
+            onChange={handlePerPage}
+          >
+            <MenuItem value={5}>5개씩 보기</MenuItem>
+            <MenuItem value={10}>10개씩 보기</MenuItem>
+            <MenuItem value={20}>20개씩 보기</MenuItem>
+          </Select>
+        </FormControl>
       </BoardHeader>
       <BoardList articles={res?.data.result.articles} />
-      <Pagination
-        length={Math.ceil(res?.data.result.articles.length / Number(perPage))}
-        handler={pageNumber => handlePageNavigate(pageNumber)}
-      />
+      <Pagination length={TOTAL_PAGE} handler={pageNumber => handlePageNavigate(pageNumber)} />
     </BoardContainer>
   );
 }
