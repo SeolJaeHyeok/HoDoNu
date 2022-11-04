5f249657085d36bbb3d5b9e6bc68ff3a4d994dfe
milkboy2564 <milkboy2564@mcircle.biz>
2022-11-04 10:17:47 +0900

    feature: React Query Devtools 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/global.css';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -17,6 +18,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   );
