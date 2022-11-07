8241e111208953e19d8569ea084808eb58df7f0f
kimhojin3714 <kimhojin3714@mcircle.biz>
2022-11-07 10:30:34 +0900

    [TP-99] feat : encode => serialize로 변경하고 qs모듈 설치

--- src/apis/recruit/list.ts
+++ src/apis/recruit/list.ts
@@ -1,7 +1,9 @@
 import { instance } from '..';
 
 const recruitListApi = {
-  getRecruitData: (params?: any) => instance.get(`/jobs`, params),
+  getRecruitData: (params?: any) => {
+    return instance.get(`/jobs`, { params });
+  },
   getRecruitTagData: () => instance.get(`/tags`),
 };
 
