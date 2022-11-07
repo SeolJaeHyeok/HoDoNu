2b09e4d80638b2f8b46b0b7c12fae2d71bc10a8c
milkboy2564 <milkboy2564@mcircle.biz>
2022-11-07 14:21:09 +0900

    [TP-45] feature: Tag 등록 페이지

--- src/pages/recruit/create/[id]/index.tsx
+++ src/pages/recruit/create/[id]/index.tsx
@@ -1,29 +1,12 @@
 import recruitApi from '@apis/recruit';
 import styled from '@emotion/styled';
-// import { TagsProps } from '@interfaces/recruit';
-// import { useRouter } from 'next/router';
-// import { useState } from 'react';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
 interface TagsResponseProps {
   content: string;
   tagId: number;
-}
-
-export default function RecruitTagsPage({ tags }: any) {
-  // const router = useRouter();
-
-  // const recruitId = router.query.id;
-  // const [selectedTags, setSelectedTags] = useState<TagsProps>();
-
-  return (
-    <Container>
-      {tags.map(({ content, tagId }: TagsResponseProps) => (
-        <div key={tagId}>
-          {content} {tagId}
-        </div>
-      ))}
-    </Container>
-  );
 }
 
 export const getServerSideProps = async () => {
@@ -35,9 +18,93 @@ export const getServerSideProps = async () => {
   };
 };
 
-const Container = styled.div`
+export default function RecruitTagsPage({ tags }: any) {
+  const router = useRouter();
+  const recruitId = router.query.id;
+  const [selectedTags, setSelectedTags] = useState<boolean[]>(
+    Array.from(
+      {
+        length: tags.length,
+      },
+      () => false
+    )
+  );
+
+  // Tag 선택
+  const handleTagClick = (tagId: number) => {
+    setSelectedTags(prev => {
+      const newArr = [...prev];
+      newArr[tagId - 1] = !newArr[tagId - 1];
+      return [...newArr];
+    });
+  };
+
+  // 선택한 tag 목록 POST
+  const handleTagPost = () => {
+    const tags: number[] = [];
+
+    selectedTags.forEach((selected, i) => {
+      if (selected) {
+        tags.push(i + 1);
+      } else return;
+    });
+
+    console.log(recruitId, tags);
+  };
+
+  return (
+    <>
+      <Container>
+        <TagTitle>관련 태그</TagTitle>
+        {tags.map(({ content, tagId }: TagsResponseProps) => (
+          <TagButton
+            isSelected={selectedTags[tagId - 1]}
+            onClick={() => handleTagClick(tagId)}
+            key={tagId}
+          >
+            {content}
+          </TagButton>
+        ))}
+      </Container>
+      <ButtonContainer>
+        <Button onClick={handleTagPost} sx={{ width: '450px' }} variant="outlined">
+          등록하기
+        </Button>
+      </ButtonContainer>
+    </>
+  );
+}
+
+const Container = styled.section`
   width: 450px;
   margin: 0 auto;
   margin-top: 30px;
   margin-bottom: 30px;
+`;
+
+const TagTitle = styled.h1`
+  border-bottom: 1px solid #e4e4e4;
+  font-size: 1.5rem;
+  text-align: center;
+  padding-bottom: 5px;
+  margin-bottom: 20px;
+`;
+
+const TagButton = styled.button<{ isSelected: boolean }>`
+  padding: 3px 16px;
+  font-size: 16px;
+  line-height: 32px;
+  letter-spacing: -0.5px;
+  border-radius: 20px;
+  background-color: ${props => (props.isSelected ? '#00dd6d' : 'white')};
+  font-weight: bold;
+  border: 1px solid rgb(228, 228, 228);
+  color: ${props => (props.isSelected ? 'white' : 'rgb(68, 68, 68)')};
+  margin: 0 10px 10px 0;
+  cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
 `;
