c96766c0c4f4166d34462b4e5cd620c84407d52e
yeseul119 <yslee@mcircle.biz>
2022-11-08 16:10:56 +0900

    [TP-38] fix lint error

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import Head from 'next/head';
 import NavBar from './NavBar';
-import CustomSideBar from '@components/SideBar/CustomSideBar';
+// import CustomSideBar from '@components/SideBar/CustomSideBar';
 
 interface LayoutProps {
   children: ReactNode;
