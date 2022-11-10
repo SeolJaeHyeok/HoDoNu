a43a9cf3c42cf41db943d2719bb6dc1f2270af3a
milkboy2564 <milkboy2564@mcircle.biz>
2022-11-10 10:19:46 +0900

    [TP-40] feature: admin user 검색 UI 완성

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -2,28 +2,36 @@
 import UserHeader from '@components/admin/users/UserHeader';
 import UserTable from '@components/admin/users/UserTable';
 import styled from '@emotion/styled';
-import useDebounce from '@hooks/useDebounce';
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import { Container } from '@mui/system';
-import { useState } from 'react';
+import React, { useState } from 'react';
+// import { debounce } from 'lodash';
 
 export default function AdminUser() {
   const [searchQueryKey, setSearchQueryKey] = useState('name');
   const [searchQuery, setSearchQuery] = useState('');
-  const debouncedQuery = useDebounce(searchQuery, 200);
 
-  const handleQueryKeyChange = (event: SelectChangeEvent) => {
+  const handleChangeQueryKey = (event: SelectChangeEvent) => {
     setSearchQueryKey(event.target.value as string);
   };
 
-  const handleChangeSearchInput = (e: any) => {
+  // const debounceSearchQuery = debounce((value) => {
+  //   setSearchQuery(value);
+  // }, 200);
+
+  const handleChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // debounceSearchQuery(e.target.value);
     setSearchQuery(e.target.value);
   };
 
-  const handleSearchSubmit = (e: any) => {
+  // TODO: 검색 결과 보여주기
+  const handleSubmitSearchInput = (e: any) => {
     e.preventDefault();
-    console.log(searchQueryKey, debouncedQuery);
+    console.log(searchQueryKey, searchQuery);
+    setSearchQuery('');
   };
+
   return (
     <div>
       <Container
@@ -33,22 +41,33 @@ export default function AdminUser() {
           alignItems: 'center',
         }}
       >
-        <FormControl sx={{ width: 150 }}>
-          <InputLabel id="demo-simple-select-label">이름</InputLabel>
+        <FormControl sx={{ width: 150, marginRight: '10px' }}>
+          <InputLabel id="demo-simple-select-label">Search By</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
             value={searchQueryKey}
             label="Age"
-            onChange={handleQueryKeyChange}
+            onChange={handleChangeQueryKey}
+            sx={{
+              height: '40px',
+            }}
           >
             <MenuItem value={'name'}>이름</MenuItem>
             <MenuItem value={'job'}>직업</MenuItem>
             <MenuItem value={'createdAt'}>시간</MenuItem>
           </Select>
         </FormControl>
-        <SearchBarForm onSubmit={handleSearchSubmit}>
-          <SearchInput onChange={handleChangeSearchInput} placeholder="검색어를 입력해주세요" />
+        <SearchBarForm onSubmit={handleSubmitSearchInput}>
+          <SearchInput
+            value={searchQuery || ''}
+            onChange={handleChangeSearchInput}
+            placeholder="검색어를 입력해주세요"
+          />
+          <SearchIcon
+            onClick={handleSubmitSearchInput}
+            sx={{ position: 'absolute', right: '0', top: '20%', cursor: 'pointer' }}
+          />
         </SearchBarForm>
       </Container>
       <UserHeader />
@@ -61,16 +80,17 @@ export default function AdminUser() {
 const SearchBarForm = styled.form`
   position: relative;
   display: flex;
-  width: 400px;
-  height: 56px;
-  border: 1px solid #a3a3a3;
-  border-radius: 6px;
+  width: 300px;
+  height: 40px;
+  border: 1px solid #c4c4c4;
+  border-radius: 4px;
   margin-left: 20px;
   margin: auto 0;
 `;
+
 const SearchInput = styled.input`
   border: none;
-  width: 80%;
+  width: 85%;
   outline: none;
   font-size: 16px;
   padding-left: 10px;
