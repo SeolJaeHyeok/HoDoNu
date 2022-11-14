85171de7549755f9ec7ae8aa56b9e2f0b00056de
kimhojin3714 <kimhojin3714@mcircle.biz>
2022-11-14 17:25:28 +0900

    [TP-140] feat : 쪽지 보내기 기능 완성

--- src/components/article/ArticleUserInfo.tsx
+++ src/components/article/ArticleUserInfo.tsx
@@ -1,21 +1,95 @@
 import CustomAvatarImage from '@components/CustomAvartar';
+import CustomModal from '@components/modal/CustomModal';
 import styled from '@emotion/styled';
+import { IconButton, Menu } from '@mui/material';
 import { convertTime } from '@utils/func';
+import { ChangeEvent, MouseEvent, useState } from 'react';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+import { useMutation } from '@tanstack/react-query';
+import detailApi from '@apis/board/detail';
 
 interface ArticleUserInfoProps {
   content: {
     createdAt: string;
     user: {
+      userId: string;
       imgUrl: string;
       nickname: string;
+      email: string;
     };
   };
 }
 
+interface sendMessageProps {
+  title: string;
+  content: string;
+}
 export default function ArticleUserInfo({ content }: ArticleUserInfoProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [sendMessage, setSendMessage] = useState<sendMessageProps>({
+    title: '',
+    content: '',
+  });
+
+  const open = Boolean(anchorEl);
+
+  const handleMenuClick = (e: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setSendMessage({
+      ...sendMessage,
+      [name]: value,
+    });
+  };
+  const requsetSendMessage = useMutation(detailApi.postSendMessage);
+
+  const handleClickSendMessage = () => {
+    requsetSendMessage.mutate({ takerId: content?.user.userId, msg: sendMessage });
+  };
+
   return (
     <CommentContainer>
-      <CustomAvatarImage src={content?.user.imgUrl} />
+      <IconButton onClick={handleMenuClick}>
+        <CustomAvatarImage src={content?.user.imgUrl} />
+      </IconButton>
+      <Menu sx={{ width: '400px' }} anchorEl={anchorEl} open={open} onClose={handleMenuClose}>
+        <ModalWrapper style={{ width: '100%' }}>
+          <CustomModal btnContent={'쪽지 보내기'} btnStyle={{ width: '100%' }}>
+            <Receiver>받는 사람: {content?.user.email}</Receiver>
+            <TextareaAutosize
+              name="title"
+              onChange={handleChangeText}
+              aria-label="empty textarea"
+              placeholder="제목을 입력해주세요"
+              style={{ width: '100%', border: '1px solid black', resize: 'none' }}
+            />
+            <TextareaAutosize
+              name="content"
+              onChange={handleChangeText}
+              aria-label="empty textarea"
+              placeholder="메시지를 입력해주세요"
+              style={{
+                width: '100%',
+                height: '70%',
+                border: '1px solid black',
+                resize: 'none',
+                fontSize: '18px',
+              }}
+            />
+            <Button variant="contained" onClick={handleClickSendMessage}>
+              전송
+            </Button>
+          </CustomModal>
+        </ModalWrapper>
+      </Menu>
       <ContentContainer>
         <NameContent>{content?.user.nickname}</NameContent>
         <TimeContent>{convertTime(content?.createdAt)}</TimeContent>
@@ -41,4 +115,11 @@ const TimeContent = styled.p`
   font-size: 13px;
   color: #6a7280;
   width: 70px;
+`;
+
+const ModalWrapper = styled.div``;
+
+const Receiver = styled.h2`
+  font-size: 20px;
+  text-align: 'center';
 `;
