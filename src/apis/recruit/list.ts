32e5f25a263413ef528ad2998f7da0b2768cd102
kimhojin3714 <kimhojin3714@mcircle.biz>
2022-11-29 16:02:20 +0900

    [TP-253] fix : list api 수정

--- src/apis/recruit/list.ts
+++ src/apis/recruit/list.ts
@@ -1,11 +1,11 @@
 import { instance } from '..';
 
 const recruitListApi = {
-  getRecruitData: (params?: any) => instance.get(`/jobs`, { params }).then(res => res.data.result),
+  getRecruitData: (params?: any) => instance.get(`/jobs`, { params }).then(res => res.data),
   getRecruitAllData: (paths: any) => {
     return instance.get(`/jobs?${paths}`);
   },
-  getRecruitTagData: () => instance.get(`/tags`),
+  getRecruitTagData: () => instance.get(`/tags`).then(res => res.data),
 };
 
 export default recruitListApi;
