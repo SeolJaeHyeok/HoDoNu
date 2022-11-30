ca8e8eff679085e48262211dd42030d5ef17515f
milkboy2564 <milkboy2564@mcircle.biz>
2022-11-30 09:05:40 +0900

    [TP-254] fix: 채용 상세페이지 SSG 방식으로 변경

--- src/pages/recruit/[id].tsx
+++ src/pages/recruit/[id].tsx
@@ -3,13 +3,13 @@ import { Divider, Typography, Box, Button, Stack } from '@mui/material';
 import { recruitApi } from '@apis/recuit';
 import ImageCarousel from '@components/recruit/detail/ImageCarousel';
 import CompanyInfo from '@components/recruit/detail/CompanyInfo';
-import { RecruitContent } from '@interfaces/recruit/detail';
+import { getDetailRes, RecruitContent } from '@interfaces/recruit/detail';
 import Contact from '@components/recruit/detail/Contact';
 import Content from '@components/recruit/detail/Content';
 import Tags from '@components/recruit/detail/Tags';
 import { useRecoilValue } from 'recoil';
 import { userInfoState } from '@atoms/userAtom';
-import { useMutation } from '@tanstack/react-query';
+import { dehydrate, QueryClient, useMutation, useQuery } from '@tanstack/react-query';
 
 export interface ParamProps {
   params: {
@@ -22,9 +22,39 @@ export interface RecruitDetailProps {
   articleId: number;
 }
 
-export default function RecruitDetail(props: RecruitDetailProps) {
+export const getStaticPaths = async () => {
+  const res = await recruitApi.getAll();
+  const posts = res.data.result[0].map(v => ({ params: { id: v.jobId.toString() } }));
+
+  return {
+    paths: posts,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }: ParamProps) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(['recruit', 'detail', params.id], () =>
+    recruitApi.getOne(params.id)
+  );
+
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      fallback: true,
+    },
+  };
+};
+
+export default function RecruitDetail() {
   const router = useRouter();
-  const { content, articleId } = props;
+  const { data: content } = useQuery<getDetailRes>(['recruit', 'detail', router.query.id]);
+
   const curUser = useRecoilValue(userInfoState);
   const curUserId = curUser?.userId;
 
@@ -39,62 +69,46 @@ export default function RecruitDetail(props: RecruitDetailProps) {
   });
 
   const handleDeleteBoard = () => {
-    deleteRecruit.mutate(articleId);
+    if (router.query.id) {
+      deleteRecruit.mutate(Number(router.query.id));
+    }
   };
 
   return (
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-      sx={{
-        width: '100%',
-      }}
-    >
-      <ImageCarousel images={content.images} />
-      <Box sx={{ py: 1, width: { sm: '650px', xs: '320px' } }}>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          {content.title}
-        </Typography>
-        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-          {content.company}
-        </Typography>
-      </Box>
-      <Tags tags={content.tags} />
-      <Divider textAlign="left" sx={{ width: { sm: '650px', xs: '320px' }, fontWeight: '500' }}>
-        채용 정보
-      </Divider>
-      <Box>
-        <Content content={content.content} />
-        <CompanyInfo address={content.address} company={content.company} />
-        <Contact user={content.user} contact={content.contact} />
-        {curUserId === content.user.userId && (
-          <Button variant="outlined" onClick={handleDeleteBoard} sx={{ m: 2 }}>
-            삭제
-          </Button>
-        )}
-      </Box>
-    </Stack>
+    content && (
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{
+          width: '100%',
+        }}
+      >
+        <ImageCarousel images={content.result.images} />
+        <Box sx={{ py: 1, width: { sm: '650px', xs: '320px' } }}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            {content.result.title}
+          </Typography>
+          <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+            {content.result.company}
+          </Typography>
+        </Box>
+        <Tags tags={content.result.tags} />
+        <Divider textAlign="left" sx={{ width: { sm: '650px', xs: '320px' }, fontWeight: '500' }}>
+          채용 정보
+        </Divider>
+        <Box>
+          <Content content={content.result.content} />
+          <CompanyInfo address={content.result.address} company={content.result.company} />
+          <Contact user={content.result.user} contact={content.result.user.email} />
+          {curUserId === content.result.userId && (
+            <Button variant="outlined" onClick={handleDeleteBoard} sx={{ m: 2 }}>
+              삭제
+            </Button>
+          )}
+        </Box>
+      </Stack>
+    )
   );
 }
-
-export const getServerSideProps = async ({ params }: ParamProps) => {
-  const { data } = await recruitApi.getOne(params.id);
-
-  if (!params) {
-    return {
-      notFound: true,
-    };
-  }
-
-  if (!data) {
-    return { notFound: true };
-  }
-  return {
-    props: {
-      content: data.result,
-      articleId: params.id,
-    },
-  };
-};
